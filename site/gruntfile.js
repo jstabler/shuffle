f2b3b9e6da5a4f639219db288efb0a62c5ca3d52
Jacob Stabler <jstabler123@gmail.com>
2014-10-02 19:20:49 -0400

    added main files

---
+++ site/gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: false,
+          yuicompress: false,
+          optimization: 2
+        },
+        files: {
+          "css/main.css": "css/main.less"
+        }
+      },
+      production: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "css/main.css": "css/main.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['css/*.less', 'index.html'],
+        tasks: ['less'],
+        options: {
+          nospawn: true,
+          livereload: true,
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+};
